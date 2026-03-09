cf98b808eeaae9fe4f4bfee5822c118595bd9ee5
Akash Gupta <140746971+akash-gupta06@users.noreply.github.com>
2026-03-09 11:30:47 +0530

    Add files via upload

---
+++ String/basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+int main()
+{
+    // char s[] = {'h', 'e', 'l', 'l', 'o', '\0', 'a'};
+    // cout << s << endl;
+    char *s = "Hello";
+    string s = "Hello";
+    cout << s << endl;
+
+    return 0;
+}
